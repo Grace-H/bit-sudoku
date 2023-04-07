b8d7ce64c53a5e48481036d72662576f7cba8dcc
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-04-06 23:36:23 -0500

    parse file

--- sudoku.c
+++ sudoku.c
@@ -4,8 +4,9 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
-#define CELL_SZ 9
+#define ROWLEN 9
 
 #define LOG(format, ...) fprintf(stderr, "%s(%d):\t" format "\n", \
 				 __func__, __LINE__, ##__VA_ARGS__)
@@ -18,13 +19,40 @@ int main(int argc, char **argv) {
   }
 
   // Initialize bitvectors of cell possibilites
-  uint16_t cells[CELL_SZ][CELL_SZ];
-  for (int i = 0; i < CELL_SZ; i++) {
-    for (int j = 0; j < CELL_SZ; j++) {
-      cells[i][j] = (1 << CELL_SZ) - 1;
-      LOG("%d", cells[i][j]);
+  uint16_t cells[ROWLEN][ROWLEN];
+  for (int i = 0; i < ROWLEN; i++) {
+    for (int j = 0; j < ROWLEN; j++) {
+      cells[i][j] = (1 << ROWLEN) - 1;
     }
   }
-  
+
+  // Read & parse puzzle from file
+  FILE *f = fopen(argv[1], "r");
+  if (!f) {
+    perror("open");
+    return 1;
+  }
+
+  char buf[ROWLEN];
+  for (int i = 0; i < ROWLEN; i++) {
+    int ret = fscanf(f, "%9c\n", buf);
+    if (ret < 1) {
+      perror("fgets");
+      return 1;
+    }
+    
+    for (int j = 0; j < ROWLEN; j++) {
+      if (buf[j] != ' ') {
+	char d = buf[j];
+	if (d >= '1' && d <= '9') {
+	  cells[i][j] = 1 << (d - '1');
+	} else {
+	  fprintf(stderr, "Invalid digit: %d\n", d);
+	  return 1;
+	}
+      }
+    }
+    
+  }
   return 0;
 }
