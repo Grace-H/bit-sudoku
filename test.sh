49a68055b0998bbe110fdc7a67fcd2e91beba7d5
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-05-10 21:43:54 -0700

    script for solving directory of test puzzles

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+total=0
+pass=0
+
+for file in $1/*
+do
+    ((total++))
+    if ./sudoku $file &> /dev/null; then
+	((pass++))
+    else
+	echo "Test failed: $file"
+    fi
+done
+
+echo "==== SUMMARY ===="
+echo "Tests passed: $pass/$total"
