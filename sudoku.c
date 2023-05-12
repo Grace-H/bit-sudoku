4750a8790f0706d16b7c857f07d441a7edfa3c01
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-05-12 10:49:47 -0700

    rename ROW to GRP_SZ

--- sudoku.c
+++ sudoku.c
@@ -8,22 +8,22 @@
 #include <string.h>
 #include <errno.h>
 
-#define ROW 9
+#define GRP_SZ 9
 #define CELL 3
 #define LOG(format, ...) fprintf(stderr, "%s(%d):\t" format "\n",       \
 				 __func__, __LINE__, ##__VA_ARGS__)
 
-static int cells_str(const uint16_t cells[ROW][ROW], char *buf, int n) {
-  if (n < (ROW + 1) * ROW) {
+static int cells_str(const uint16_t cells[GRP_SZ][GRP_SZ], char *buf, int n) {
+  if (n < (GRP_SZ + 1) * GRP_SZ) {
     LOG("too short");
     errno = EINVAL;
     return -1;
   }
 
   int k = 0;
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < ROW + 1; j++) {
-      if (j == ROW) {
+  for (int i = 0; i < GRP_SZ; i++) {
+    for (int j = 0; j < GRP_SZ + 1; j++) {
+      if (j == GRP_SZ) {
         buf[k++] = '\n';
       }
       else if (!(cells[i][j] & (cells[i][j] - 1))) {
@@ -41,42 +41,48 @@ static int cells_str(const uint16_t cells[ROW][ROW], char *buf, int n) {
   return k;
 }
 
-// Get square number (0->9 reading left-right top-bottom) from i,j coordinates
-// Square index is i rounded down to nearest multiple of cell size + j divided by cell size
-static inline int sqr_index(int i, int j) {
-  return (i / CELL) * CELL + j / CELL;
-}
-
-// Count number of set bits in vector of length ROW
-static int bit_count(const uint16_t n) {
-  int count = 0;
-  for (int i = 0; i < ROW; i++) {
-    count += (n >> i) & 1;
-  }
-  return count;
-}
-
 static int vec_str(const uint16_t vec, char *buf, int n) {
-  if (n < ROW + 1) {
+  if (n < GRP_SZ + 1) {
     errno = EINVAL;
     return -1;
   }
 
   int i;
-  for (i = 0; i < ROW; i++) {
-    buf[i] = ((vec >> (ROW - i - 1)) & 1) + '0';
+  for (i = 0; i < GRP_SZ; i++) {
+    buf[i] = ((vec >> (GRP_SZ - i - 1)) & 1) + '0';
   }
 
   buf[i] = '\0';
   return i;
 }
 
+// Get square number (0->9 reading left-right top-bottom) from i,j coordinates
+// Square index is i rounded down to nearest multiple of cell size + j divided by cell size
+static inline int sqr_index(int i, int j) {
+  return (i / CELL) * CELL + j / CELL;
+}
+
+// Get i,j coordinates of top left cell in a square from its index
+static void sqr_coords(int n, int *i, int *j) {
+  *i = (n / CELL) * CELL;
+  *j = (n % CELL) * CELL;
+}
+
+// Count number of set bits in vector of length GRP_SZ
+static int bit_count(const uint16_t n) {
+  int count = 0;
+  for (int i = 0; i < GRP_SZ; i++) {
+    count += (n >> i) & 1;
+  }
+  return count;
+}
+
 // Update which values in each row, column, and square have been solved
 // Set bit indicates value exists in region
-static void update_solved(const uint16_t cells[ROW][ROW], uint16_t row[ROW],
-			  uint16_t col[ROW], uint16_t sqr[ROW]) {
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < ROW; j++) {
+static void update_solved(const uint16_t cells[GRP_SZ][GRP_SZ], uint16_t row[GRP_SZ],
+			  uint16_t col[GRP_SZ], uint16_t sqr[GRP_SZ]) {
+  for (int i = 0; i < GRP_SZ; i++) {
+    for (int j = 0; j < GRP_SZ; j++) {
       uint16_t c = cells[i][j];
       // if only one number is in bitvector (power of 2), mark it as found
       if (!(c & (c - 1))) {
@@ -90,9 +96,9 @@ static void update_solved(const uint16_t cells[ROW][ROW], uint16_t row[ROW],
 
 // Check if board is solved - each row/column/square is solved
 // if the xor of all cells is 0x1ff (1 bit set)
-int is_solved(uint16_t row[ROW], uint16_t col[ROW], uint16_t sqr[ROW]) {
-  uint16_t target = (1 << ROW) - 1;
-  for (int i = 0; i < ROW; i++) {
+int is_solved(uint16_t row[GRP_SZ], uint16_t col[GRP_SZ], uint16_t sqr[GRP_SZ]) {
+  uint16_t target = (1 << GRP_SZ) - 1;
+  for (int i = 0; i < GRP_SZ; i++) {
     if (row[i] != target || col[i] != target || sqr[i] != target) {
       return 0;
     }
@@ -102,10 +108,10 @@ int is_solved(uint16_t row[ROW], uint16_t col[ROW], uint16_t sqr[ROW]) {
 
 // Eliminate possibilites for each cell based on what values are already
 // in each row/column/square
-static void eliminate(uint16_t cells[ROW][ROW], const uint16_t row[ROW],
-		      const uint16_t col[ROW], const uint16_t sqr[ROW]) {
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < ROW; j++) {
+static void eliminate(uint16_t cells[GRP_SZ][GRP_SZ], const uint16_t row[GRP_SZ],
+		      const uint16_t col[GRP_SZ], const uint16_t sqr[GRP_SZ]) {
+  for (int i = 0; i < GRP_SZ; i++) {
+    for (int j = 0; j < GRP_SZ; j++) {
       // if this cell is not solved, cross off possibilities
       if(cells[i][j] & (cells[i][j] - 1)) {
         cells[i][j] &= ~row[i];
@@ -118,17 +124,17 @@ static void eliminate(uint16_t cells[ROW][ROW], const uint16_t row[ROW],
 
 // Use naked pairs strategy to eliminate further options
 // Naked pair: two cells in same group that have only two identical possibilities
-static void naked_pairs(uint16_t cells[ROW][ROW]) {
+static void naked_pairs(uint16_t cells[GRP_SZ][GRP_SZ]) {
 
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < ROW; j++) {
+  for (int i = 0; i < GRP_SZ; i++) {
+    for (int j = 0; j < GRP_SZ; j++) {
       if (bit_count(cells[i][j]) == 2) {
         // Check for pairs in remainder of row, eliminating possibiliities
         // from row if so
-        for (int k = j + 1; k < ROW; k++) {
+        for (int k = j + 1; k < GRP_SZ; k++) {
           // same pair
           if (cells[i][j] == cells[i][k]) {
-            for (int x = 0; x < ROW; x++) {
+            for (int x = 0; x < GRP_SZ; x++) {
               if (x != j && x != k) {
                 cells[i][x] &= ~cells[i][j];
               }
@@ -138,9 +144,9 @@ static void naked_pairs(uint16_t cells[ROW][ROW]) {
         }
 
         // Column
-        for (int k = i + 1; k < ROW; k++) {
+        for (int k = i + 1; k < GRP_SZ; k++) {
           if (cells[i][j] == cells[k][j]) {
-            for (int y = 0; y < ROW; y++) {
+            for (int y = 0; y < GRP_SZ; y++) {
               if (y != i && y != k) {
                 cells[y][j] &= ~cells[i][j];
               }
@@ -177,10 +183,10 @@ int main(int argc, char **argv) {
   }
 
   // Initialize bitvectors of cell possibilites
-  uint16_t cells[ROW][ROW]; // only the first 9 bits will be used
-  for (int i = 0; i < ROW; i++) {
-    for (int j = 0; j < ROW; j++) {
-      cells[i][j] = (1 << ROW) - 1;
+  uint16_t cells[GRP_SZ][GRP_SZ]; // only the first 9 bits will be used
+  for (int i = 0; i < GRP_SZ; i++) {
+    for (int j = 0; j < GRP_SZ; j++) {
+      cells[i][j] = (1 << GRP_SZ) - 1;
     }
   }
 
@@ -191,15 +197,15 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  char buf[ROW];
-  for (int i = 0; i < ROW; i++) {
+  char buf[GRP_SZ];
+  for (int i = 0; i < GRP_SZ; i++) {
     int ret = fscanf(f, "%9c\n", buf);
     if (ret < 1) {
       perror("fgets");
       return 1;
     }
 
-    for (int j = 0; j < ROW; j++) {
+    for (int j = 0; j < GRP_SZ; j++) {
       if (buf[j] != '0') {
         char d = buf[j];
         if (d >= '1' && d <= '9') {
@@ -213,22 +219,22 @@ int main(int argc, char **argv) {
   }
 
   // Cross off initial round of bits
-  uint16_t rowfin[ROW];
-  uint16_t colfin[ROW];
-  uint16_t sqrfin[ROW];
+  uint16_t rowfin[GRP_SZ];
+  uint16_t colfin[GRP_SZ];
+  uint16_t sqrfin[GRP_SZ];
 
-  memset(&rowfin, 0, ROW * sizeof(uint16_t));
-  memset(&colfin, 0, ROW * sizeof(uint16_t));
-  memset(&sqrfin, 0, ROW * sizeof(uint16_t));
+  memset(&rowfin, 0, GRP_SZ * sizeof(uint16_t));
+  memset(&colfin, 0, GRP_SZ * sizeof(uint16_t));
+  memset(&sqrfin, 0, GRP_SZ * sizeof(uint16_t));
 
   update_solved(cells, rowfin, colfin, sqrfin);
 
   for (int i = 0; i < 20; i++) {
     // Do one round of elimination
     eliminate(cells, rowfin, colfin, sqrfin);
-    char buf[ROW * ROW + ROW];
-    cells_str(cells, buf, ROW * ROW + ROW);
-    LOG("cells after eliminate round %d\n%s", i, buf);
+    //    char buf[GRP_SZ * GRP_SZ + GRP_SZ];
+    //    cells_str(cells, buf, GRP_SZ * GRP_SZ + GRP_SZ);
+    //    LOG("cells after eliminate round %d\n%s", i, buf);
     naked_pairs(cells);
     cells_str(cells, buf, ROW * ROW + ROW);
     LOG("cells after naked pairs \n%s", buf);
