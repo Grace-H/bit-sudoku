e2a5ab4bd57c2702db3e54ae2b6ce9b883e9d1fc
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-06-29 22:19:28 -0500

    script to run few tests of each level

---
+++ test-heads.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "--> 1.2"
+./test.sh tests/head1_2/
+
+echo "--> 1.5"
+./test.sh tests/head1_5/
+
+echo "--> 1.7"
+./test.sh tests/head1_7/
+
+echo "--> 2.0"
+./test.sh tests/head2_0/
+
+echo "--> 2.3"
+./test.sh tests/head2_3/
+
