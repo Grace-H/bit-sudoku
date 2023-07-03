5c38e6350ec55ceac5900a5835dce2aced584239
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-07-02 19:03:39 -0500

    add 3.x problems to tests

--- test-heads.sh
+++ test-heads.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 echo "--> 1.2"
-./test.sh tests/head1_2/
+./test.sh tests/head1_2
 
 echo "--> 1.5"
-./test.sh tests/head1_5/
+./test.sh tests/head1_5
 
 echo "--> 1.7"
-./test.sh tests/head1_7/
+./test.sh tests/head1_7
 
 echo "--> 2.0"
-./test.sh tests/head2_0/
+./test.sh tests/head2_0
 
 echo "--> 2.3"
-./test.sh tests/head2_3/
+./test.sh tests/head2_3
 
+echo "--> 2.5"
+./test.sh tests/head2_5
+
+echo "--> 2.6"
+./test.sh tests/head2_6
+
+echo "--> 2.8"
+./test.sh tests/head2_8
+
+echo "--> 3.0"
+./test.sh tests/head3_0
+
+echo "--> 3.2"
+./test.sh tests/head3_2
+
+echo "--> 3.4"
+./test.sh tests/head3_4
+
+echo "--> 3.6"
+./test.sh tests/head3_6
+
+echo "--> 3.8"
+./test.sh tests/head3_8
