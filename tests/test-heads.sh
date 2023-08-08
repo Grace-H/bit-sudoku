37a2c49fdb73775671d2f0ed276e6f8ba1ff2010
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-08-07 21:56:32 -0500

    loop over array in larger test

--- tests/test-heads.sh
+++ tests/test-heads.sh
@@ -1,40 +1,14 @@
 #!/bin/bash
 
-echo "--> 1.2"
-./test.sh $1 head1_2
+# Run 10 tests from each level and print results
+# Call from parent directory
 
-echo "--> 1.5"
-./test.sh $1 head1_5
+# Usage: test-heads.sh <solver>
 
-echo "--> 1.7"
-./test.sh $1 head1_7
+heads=( 1_2 1_5 1_7 2_0 2_3 2_5 2_6 2_8 3_0 3_2 3_4 3_6 3_8 )
 
-echo "--> 2.0"
-./test.sh $1 head2_0
-
-echo "--> 2.3"
-./test.sh $1 head2_3
-
-echo "--> 2.5"
-./test.sh $1 head2_5
-
-echo "--> 2.6"
-./test.sh $1 head2_6
-
-echo "--> 2.8"
-./test.sh $1 head2_8
-
-echo "--> 3.0"
-./test.sh $1 head3_0
-
-echo "--> 3.2"
-./test.sh $1 head3_2
-
-echo "--> 3.4"
-./test.sh $1 head3_4
-
-echo "--> 3.6"
-./test.sh $1 head3_6
-
-echo "--> 3.8"
-./test.sh $1 head3_8
+for head in "${heads[@]}"
+do
+    echo "--> ${head}"
+    tests/test.sh $1 "tests/head${head}"
+done
