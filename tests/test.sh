803108a4f4a86a7dde83a126348a40a695a0592d
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-09-08 23:20:13 -0500

    make base test quieter

--- tests/test.sh
+++ tests/test.sh
@@ -2,16 +2,29 @@
 
 total=0
 pass=0
+verbose=false
+
+while getopts "hv" OPT; do
+    case $OPT in
+	h)
+	    echo "Usage: test.sh [-hv] <solver> <test-dir>"
+	    exit 1
+	    ;;
+	v)
+	    verbose=true
+	    ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 for file in $2/*
 do
     ((total++))
-    if $1 $file &> /dev/null; then
+    if $1 $file &> /dev/null ; then
         ((pass++))
-    else
+    elif [ $verbose = true ] ; then
         echo "Test failed: $file"
     fi
 done
 
-echo "==== SUMMARY ===="
-echo "Tests passed: $pass/$total"
+echo "($(basename $2)) Tests Passed: $pass/$total"
