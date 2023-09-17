4308c182f3dec3cd9426ce736010fa1b813afc07
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2023-09-16 21:21:54 -0500

    print failed tests in shorter test suite

--- tests/test-heads.sh
+++ tests/test-heads.sh
@@ -10,5 +10,5 @@ heads=( 1_2 1_5 1_7 2_0 2_3 2_5 2_6 2_8 3_0 3_2 3_4 3_6 3_8 )
 for head in "${heads[@]}"
 do
     echo "--> ${head}"
-    tests/test.sh $1 "tests/head${head}"
+    tests/test.sh -v $1 "tests/head${head}"
 done
