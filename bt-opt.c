ec1cf5a3b5422ae37b1870b861b6f0dda331c609
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2024-01-11 22:36:08 -0600

    reduce possible branches, add backtrack counting

--- bt-opt.c
+++ bt-opt.c
@@ -29,7 +29,7 @@ static void blk_coords(int n, int *i, int *j) {
  * 	contain values from the original puzzle
  * @return nonzero on failure to solve
  */
-int solve(uint16_t cells[HOUSE_SZ][HOUSE_SZ], uint16_t original[HOUSE_SZ]) {
+int solve(uint16_t cells[HOUSE_SZ][HOUSE_SZ], uint16_t original[HOUSE_SZ][HOUSE_SZ]) {
   int backtracks = 0;
   uint16_t max = 1 << (HOUSE_SZ + 1);
 
@@ -59,7 +59,7 @@ int solve(uint16_t cells[HOUSE_SZ][HOUSE_SZ], uint16_t original[HOUSE_SZ]) {
     int z = blk_index(i, j);
 
     // If not part of the original puzzle
-    if (!((original[i] >> j) & 1)) {
+    if (original[i][j]) {
       if (cells[i][j] > 1) {
         row[i] ^= cells[i][j];
         col[j] ^= cells[i][j];
@@ -69,7 +69,10 @@ int solve(uint16_t cells[HOUSE_SZ][HOUSE_SZ], uint16_t original[HOUSE_SZ]) {
       while (cells[i][j] < max) {
         cells[i][j] <<= 1;
         // No conflict with existing solved cells
-        if (!(cells[i][j] & row[i] || cells[i][j] & col[j] || cells[i][j] & blk[z])) {
+        if (!(cells[i][j] & row[i] || 
+              cells[i][j] & col[j] || 
+              cells[i][j] & blk[z]) && 
+            (original[i][j] & cells[i][j])) {
           row[i] |= cells[i][j];
           col[j] |= cells[i][j];
           blk[z] |= cells[i][j];
@@ -78,6 +81,7 @@ int solve(uint16_t cells[HOUSE_SZ][HOUSE_SZ], uint16_t original[HOUSE_SZ]) {
         }
       }
       if (cells[i][j] >= max) {
+        backtracks++;
         cells[i][j] = 1;
         delta = -1;
       }
@@ -98,6 +102,7 @@ int solve(uint16_t cells[HOUSE_SZ][HOUSE_SZ], uint16_t original[HOUSE_SZ]) {
     return 1;
   }
   fprintf(stderr, "%d\n", backtracks);
+
   return 0;
 }
 
@@ -108,12 +113,13 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // Initialize all cells to 0
-  uint16_t cells[HOUSE_SZ][HOUSE_SZ];
-  uint16_t original[HOUSE_SZ];
+  // Initialize all cells
+  uint16_t cells[HOUSE_SZ][HOUSE_SZ];  // Current solution for all cells
+  uint16_t original[HOUSE_SZ][HOUSE_SZ];  // Candidates based on initial puzzle
+  uint16_t max = (1 << (HOUSE_SZ + 1)) - 2;
   for (int i = 0; i < HOUSE_SZ; i++) {
-    original[i] = 0;
     for (int j = 0; j < HOUSE_SZ; j++) {
+      original[i][j] = max;
       cells[i][j] = 1;
     }
   }
@@ -138,24 +144,24 @@ int main(int argc, char **argv) {
       uint16_t d = buf[j] - '0';
       if (d > 0 && d <= 9) {
         cells[i][j] = 1 << d;
-        original[i] |= (1 << j);
+        original[i][j] = 0;
 
         uint16_t elim = ~cells[i][j];
         for (int x = 0; x < HOUSE_SZ; x++) {
-          cells[i][x] &= elim;
+          original[i][x] &= elim;
         }
         for (int y = 0; y < HOUSE_SZ; y++) {
-          cells[y][j] &= elim;
+          original[y][j] &= elim;
         }
         int z1, z2;
         blk_coords(blk_index(i, j), &z1, &z2);
         for (int a = z1; a < z1 + BLK_WIDTH; a++) {
           for (int b = z2; b < z2 + BLK_WIDTH; b++) {
-            cells[a][b] &= elim;
+            original[a][b] &= elim;
           }
         }
       } else if (d != 0) {
-        fprintf(stderr, "Invalid digit: %d\n", d);
+        fprintf(stderr, "Invalid digit in file: %d\n", d);
         fclose(f);
         return 1;
       }
