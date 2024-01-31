0e551a1de38eab70234508c9d79f30c2b8ba3676
Grace Hunter <39381292+Grace-H@users.noreply.github.com>
2024-01-30 20:26:33 -0600

    use larger test set for backtrack benching

--- tests/bench-backtrack.sh
+++ tests/bench-backtrack.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-# bktrk-bench.sh
+# bench-backtrack.sh
 # Print table of backtrack counts for each puzzle in AI set using various solvers
-# Usage: bktrk-bench.sh    # Call from parent of tests directory
+# Usage: bench-backtrack.sh    # Call from parent of tests directory
 
+if [[ "$(basename $PWD)" == "tests" ]] ; then
+	echo "Re-run from above tests directory."
+	echo "Usage: bench-backtrack.sh"
+	exit 1
+fi
+
+testdir="tests/sample/"
 printf "%20s %10s %10s %10s %10s\n" "File" "bt" "bt-opt" "ss" "ss-opt"
 
-for f in tests/ai/*
+for f in ${testdir}*
 do
 	bt=$(./bt ${f})
 	btopt=$(./bt-opt ${f})
 	ss=$(./ss ${f})
 	ssopt=$(./ss-opt ${f})
 
-	printf "%20s %10d %10d %10d %10d\n" ${f#tests/ai/ai-} ${bt} ${btopt} ${ss} ${ssopt}
+	printf "%20s %10d %10d %10d %10d\n" ${f#${testdir}} ${bt} ${btopt} ${ss} ${ssopt}
 done
 
